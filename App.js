604f3739095cf9dd06a4822257ace878ddc65bc2
Jose Pagan <jm.pagan@gmail.com>
2022-01-20 22:28:09 +0000

    feat: settings screen created and link icon working

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 // import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import 'react-native-get-random-values'
-
-
 import { store, persistor } from './app/store/store';
-import AppMain from './app/AppMain';
 import { NavigationContainer } from '@react-navigation/native';
-import { dirtyCardsReset } from './app/store/cardsSlice';
-import { dirtyDecksReset } from './app/store/decksSlice';
+import * as Notifications from 'expo-notifications';
+import { useState, useRef, useEffect } from 'react';
+import NotificationModal from './app/components/NotificationModal';
+import { isDevice } from 'expo-device';
+
+import AppMain from './app/AppMain';
+// import { dirtyCardsReset } from './app/store/cardsSlice';
+// import { dirtyDecksReset } from './app/store/decksSlice';
+
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+
+
 export default function App() {
   // store.dispatch(dirtyCardsReset())
   // store.dispatch(dirtyDecksReset())
   // console.log("store", store.getState())
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
           <AppMain />
+          {/* <NotificationModal notification={notification} /> */}
         </NavigationContainer>
       </PersistGate>
     </Provider>
   )
 }
+
 
--- app/AppMain.js
+++ app/AppMain.js
@@ -1,11 +1,13 @@
 // import { NavigationContainer } from "@react-navigation/native";
 import DeckListScreen from "./screens/DeckListScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { Button, Pressable } from "react-native"
 import NewDeckForm from "./screens/NewDeckScreen";
 import IndividualDeckScreen from "./screens/IndividualDeckScreen";
 import NewQuestionScreen from "./screens/NewQuestionScreen";
 import QuizScreen from "./screens/QuizScreen";
+import { Entypo } from '@expo/vector-icons';
+import Settings from "./screens/Settings";
 const Stack = createNativeStackNavigator()
 const AppMain = () => {
     return (
@@ -13,7 +15,13 @@ const AppMain = () => {
             <Stack.Screen
                 name="Decks"
                 component={DeckListScreen}
-                options={{ animation: "slide_from_right" }}
+                options={({ navigation }) => ({
+                    headerRight: () => (
+                        <Pressable onPress={() => { navigation.navigate("Settings") }}>
+                            <Entypo name="cog" size={24} color="black" />
+                        </Pressable>
+                    )
+                })}
             />
             <Stack.Screen
                 name={"New Deck"}
@@ -23,7 +31,7 @@ const AppMain = () => {
             <Stack.Screen
                 name={"Deck"}
                 component={IndividualDeckScreen}
-                options={({ route }) => ({ title: `Deck: ${route.params.name}` })}
+                options={({ route }) => ({ title: `Deck: ${route.params.name}`, animation: "slide_from_right" })}
             />
             <Stack.Screen
                 name={"New Question"}
@@ -34,6 +42,10 @@ const AppMain = () => {
                 name={"Quiz"}
                 component={QuizScreen}
             // options={({ route }) => ({ title: `Deck: ${route.params.name}` })}
+            />
+            <Stack.Screen
+                name={"Settings"}
+                component={Settings}
             />
 
         </Stack.Navigator>
